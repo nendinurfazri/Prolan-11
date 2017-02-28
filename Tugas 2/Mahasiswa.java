1839594c11092f752d353591a98e7b488b032325
nendinurfazri <nendi021@gmail.com>
2017-02-28 23:28:10 +0700

    Add files via upload

---
+++ Tugas 2/Mahasiswa.java
@@ -0,0 +1,9 @@
+public class Mahasiswa
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Nama	= Nendi Nurfazri");
+    System.out.println("NIM	= 10114498");
+    System.out.println("Alamat	= Cikalong Wetan No.20");
+  }
+}
