bd3165bb89fc87b84c3ebe31a5000b83f5399247
nendinurfazri <nendi021@gmail.com>
2017-03-07 22:39:19 +0700

    Add files via upload

---
+++ Tugas 4/main.java
@@ -0,0 +1,8 @@
+class main {
+	public static void main (String args[]) {
+		model_kit SD = new model_kit();
+		
+		SD.jenis = "Super Deformed";
+		SD.jual();
+	}
+}
---
+++ Tugas 4/model_kit.java
@@ -0,0 +1,11 @@
+public class model_kit {
+	
+	String nama, jenis;
+	int harga;
+	
+
+	public void jual () {
+		System.out.println("Rp.150000");
+		
+	}
+}
